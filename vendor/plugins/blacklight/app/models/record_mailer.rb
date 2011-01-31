77950820c3ec4dfd7e1e84f684f81065542af60a
Mark A. Matienzo <mark@matienzo.org>
2011-01-30 22:04:43 -0500

    Initial import

---
+++ vendor/plugins/blacklight/app/models/record_mailer.rb
@@ -0,0 +1,39 @@
+# Only works for documents with a #to_marc right now. 
+class RecordMailer < ActionMailer::Base
+
+  
+  def email_record(documents, details, from_host, host)
+    #raise ArgumentError.new("RecordMailer#email_record only works with documents with a #to_marc") unless document.respond_to?(:to_marc)
+    
+    recipients details[:to]
+    if documents.size == 1
+      subject "Item Record: #{documents.first.to_marc['245']['a'] rescue 'N/A'}"
+    else
+      subject "Item records"
+    end
+    from "no-reply@" << from_host
+    body :documents => documents, :host => host, :message => details[:message]
+  end
+  
+  def sms_record(documents, details, from_host, host)
+    if sms_mapping[details[:carrier]]
+      to = "#{details[:to]}@#{sms_mapping[details[:carrier]]}"
+    end
+    recipients to
+    from "no-reply@" << from_host
+    body :documents => documents, :host => host
+  end
+
+  protected
+  
+  def sms_mapping
+    {'virgin' => 'vmobl.com',
+    'att' => 'txt.att.net',
+    'verizon' => 'vtext.com',
+    'nextel' => 'messaging.nextel.com',
+    'sprint' => 'messaging.sprintpcs.com',
+    'tmobile' => 'tmomail.net',
+    'alltel' => 'message.alltel.com',
+    'cricket' => 'mms.mycricket.com'}
+  end
+end
